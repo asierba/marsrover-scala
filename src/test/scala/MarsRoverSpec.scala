93ed9608805cb68815049ad07f5cc4bb6751ecf8
Asier Barrenetxea <abarrenetxea@equalexperts.com>
2020-12-28 15:23:06 +0100

    Empty poejct with test scaffolding

---
+++ src/test/scala/MarsRoverSpec.scala
@@ -0,0 +1,8 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class MarsRoverSpec extends AnyFlatSpec with should.Matchers {
+  "A test" should "pass" in {
+    true shouldBe true
+  }
+}
