ec277be8a6745891ae7746d225a35babfa0d375b
Asier Barrenetxea <abarrenetxea@equalexperts.com>
2020-12-28 15:32:38 +0100

    Rovers reports position and direction

--- src/test/scala/MarsRoverSpec.scala
+++ src/test/scala/MarsRoverSpec.scala
@@ -2,7 +2,17 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 class MarsRoverSpec extends AnyFlatSpec with should.Matchers {
-  "A test" should "pass" in {
-    true shouldBe true
+  "MarsRover" should "reports initial coordinate and direction" in {
+    val rover = Rover(Position(4,2), Direction.EAST)
+    rover.direction shouldBe Direction.EAST
+    rover.position shouldBe Position(4,2)
   }
 }
+
+case class Position(x: Int, y: Int)
+object Direction extends Enumeration {
+  type Direction = Value
+  val EAST = Value
+}
+import Direction._
+case class Rover(position: Position, direction: Direction)
