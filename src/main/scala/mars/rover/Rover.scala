8536028b384df6652f5ece19142c3960face6061
Asier Barrenetxea <abarrenetxea@equalexperts.com>
2020-12-29 12:50:23 +0100

    refactor move methods and add type anotations

--- src/main/scala/mars/rover/Rover.scala
+++ src/main/scala/mars/rover/Rover.scala
@@ -18,26 +18,35 @@ case class Rover(position: Position, direction: Direction, obstacles: List[Posit
       rover executeCommands commands.tail
   }
 
-  private def executeCommand(command: Char) = {
-    if (stopped)
-      this
-    else
-      command match {
-        case 'L' => rotateLeft
-        case 'R' => rotateRight
-        case 'F' => moveForward
-        case 'B' => moveBackwards
-      }
+  private def executeCommand(command: Char): Rover = if (stopped)
+    this
+  else
+    command match {
+      case 'L' => rotateLeft
+      case 'R' => rotateRight
+      case 'F' => moveForward
+      case 'B' => moveBackwards
+    }
+
+  private def moveForward: Rover = moveOne(true)
+
+  private def moveBackwards: Rover = moveOne(false)
+
+  private def rotateRight: Rover = direction match {
+    case Direction.NORTH => Rover(position, Direction.EAST, obstacles, stopped)
+    case Direction.WEST => Rover(position, Direction.NORTH, obstacles, stopped)
+    case Direction.EAST => Rover(position, Direction.SOUTH, obstacles, stopped)
+    case Direction.SOUTH => Rover(position, Direction.WEST, obstacles, stopped)
   }
 
-  private def newPosition(direction: Direction, increment: Int) = direction match {
-    case Direction.NORTH => Position(position.x, position.y + increment)
-    case Direction.WEST => Position(position.x - increment, position.y)
-    case Direction.EAST => Position(position.x + increment, position.y)
-    case Direction.SOUTH => Position(position.x, position.y - increment)
+  private def rotateLeft: Rover = direction match {
+    case Direction.NORTH => Rover(position, Direction.WEST, obstacles, stopped)
+    case Direction.WEST => Rover(position, Direction.SOUTH, obstacles, stopped)
+    case Direction.EAST => Rover(position, Direction.NORTH, obstacles, stopped)
+    case Direction.SOUTH => Rover(position, Direction.EAST, obstacles, stopped)
   }
 
-  private def move(forward: Boolean) = {
+  private def moveOne(forward: Boolean): Rover = {
     val increment = if (forward) 1 else -1;
     val nextPosition = newPosition(direction, increment)
     if (obstacles.contains(nextPosition))
@@ -46,21 +55,10 @@ case class Rover(position: Position, direction: Direction, obstacles: List[Posit
       Rover(nextPosition, direction, obstacles)
   }
 
-  private def moveForward = move(true)
-
-  private def moveBackwards = move(false)
-
-  private def rotateRight = direction match {
-    case Direction.NORTH => Rover(position, Direction.EAST, obstacles, stopped)
-    case Direction.WEST => Rover(position, Direction.NORTH, obstacles, stopped)
-    case Direction.EAST => Rover(position, Direction.SOUTH, obstacles, stopped)
-    case Direction.SOUTH => Rover(position, Direction.WEST, obstacles, stopped)
-  }
-
-  private def rotateLeft = direction match {
-    case Direction.NORTH => Rover(position, Direction.WEST, obstacles, stopped)
-    case Direction.WEST => Rover(position, Direction.SOUTH, obstacles, stopped)
-    case Direction.EAST => Rover(position, Direction.NORTH, obstacles, stopped)
-    case Direction.SOUTH => Rover(position, Direction.EAST, obstacles, stopped)
+  private def newPosition(direction: Direction, increment: Int): Position = direction match {
+    case Direction.NORTH => Position(position.x, position.y + increment)
+    case Direction.WEST => Position(position.x - increment, position.y)
+    case Direction.EAST => Position(position.x + increment, position.y)
+    case Direction.SOUTH => Position(position.x, position.y - increment)
   }
 }
